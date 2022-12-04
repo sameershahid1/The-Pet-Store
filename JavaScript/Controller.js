7ae4c027eef4487132f9c36d193f2efa59578a57
Sameer Shahid <goku.hulk234@gmail.com>
2022-12-04 16:39:22 +0500

    Connecting Home and product pages completed

---
+++ JavaScript/Controller.js
@@ -0,0 +1,21 @@
+
+const MoveHome=()=>
+{
+  window.location.href="./Home.html";
+}
+
+const MoveStore=()=>
+{
+    window.location.href="./Pet-Store.html";
+}
+
+const MoveBuy=()=>
+{
+    window.location.href="./Buy-Sell.html";
+}
+
+const MoverMedicine=()=>{
+    window.location.href="./Veterianary.html";
+}
+
+
