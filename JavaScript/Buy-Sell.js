d79a6c4038000c12dfce2399febf2a532e2b6477
Sameer Shahid <goku.hulk234@gmail.com>
2022-12-04 11:16:51 +0500

    Buy/Sell page is completed

---
+++ JavaScript/Buy-Sell.js
@@ -0,0 +1,99 @@
+
+const Data=[
+{
+ id:0,
+ name:"Persian Cat",
+ url:"https://www.agentpet.com/storage/pets/w0FMMINmNXYe1dsTucDzqV6xXqxzweG71wtjmRgL.png",
+ price:"$20",
+ type:"Animal"
+
+}
+,
+{
+ id:1,
+ name:"Persian Cat",
+ url:"./Assets/Buy-Sell-Products/A1.jpeg",
+ price:"$20",
+ type:"Animal"
+}
+,
+{
+ id:2,
+ name:"Persian Cat",
+ url:"./Assets/Buy-Sell-Products/A2.jpeg",
+ price:"$20",
+ type:"Animal"
+},
+{
+ id:2,
+ name:"Persian Cat",
+ url:"./Assets/Buy-Sell-Products/A3.jpeg",
+ price:"$20",
+ type:"Animal"
+}
+,
+{
+ id:2,
+ name:"Persian Cat",
+ url:"./Assets/Buy-Sell-Products/A4.jpeg",
+ price:"$20",
+ type:"Animal"
+}
+,
+{
+ id:2,
+ name:"Persian Cat",
+ url:"./Assets/Buy-Sell-Products/A5.jpeg",
+ price:"$20",
+ type:"Animal"
+}
+,
+{
+ id:2,
+ name:"Persian Cat",
+ url:"./Assets/Buy-Sell-Products/A6.jpeg",
+ price:"$20",
+ type:"Animal"
+}
+,
+{
+ id:2,
+ name:"Persian Cat",
+ url:"./Assets/Buy-Sell-Products/A7.jpeg",
+ price:"$20",
+ type:"Animal"
+}
+,
+{
+ id:2,
+ name:"Persian Cat",
+ url:"./Assets/Buy-Sell-Products/A8.jpeg",
+ price:"$20",
+ type:"Animal"
+}
+
+];
+
+const ProductCreator=(Data)=>
+{
+    const Product=`
+    <div class='card text-center'>
+     <img class='card-img-bottom ms-1 p-2 img-size' src=${Data.url} alt=${Data.type}>
+     <div class='card-body'>
+       <h5 class="card-title">${Data.name}</h5>
+       <p class="card-text">${Data.price}</p>
+       <button class="btn btn-primary ps-4 pe-4">Buy</button>
+     </div>
+    </div>
+    `;
+    return Product;
+}
+
+
+const List=Data.map(Da=>ProductCreator(Da));
+const Buy_Sell=document.getElementById("Products");
+let Products="";
+List.map(x=>{Products+=x;})
+
+Buy_Sell.innerHTML=Products;
+
